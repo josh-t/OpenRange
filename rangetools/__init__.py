bdd8c809b20accf6ea9e05fe7c49faf4f9295ca5
Sean Wallitsch <shidarin@alphamatte.com>
2015-01-24 13:57:07 -0800

    Fleshed out setup.py with comments, storing metadata in project __init__

--- rangetools/__init__.py
+++ rangetools/__init__.py
@@ -2,3 +2,16 @@
 from .objects import Range, RangeList
 from .funcs import irange, range_str
 
+# Project Metadata
+
+__author__ = "Josh Tomlinson"
+__copyright__ = "Copyright 2014, Sean Wallitsch"
+__credits__ = [
+    "Josh Tomlinson", "Frederick LeMaster", "Julius Yang", "Victor Vazquez",
+    "Wil Ho", "Jeff Beeland", "Sanjit Patel", "Stephen Lu", "Sean Wallitsch",
+]
+__license__ = "MIT"
+__version__ = "0.0.1"
+__maintainer__ = "Josh Tomlinson"
+__email__ = "joshetomlinson@gmail.com"
+__status__ = "Pre-Alpha"
